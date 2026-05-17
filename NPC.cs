fcf6c5060c244b05f37306cfeead1f91c2166be8
Tima4708 <gtgame81470@gmail.com>
2026-05-17 11:42:19 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public int health = 5;
+
+    public int level = 2;
+
+    public float speed = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        // Изменения количества здоровья
+        health += level;
+        print("Здоровье игрока " + health);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
